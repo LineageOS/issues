f6dce13ed004d63ca017f0d40fa243daa4f81a48
LuK1337 <priv.luk@gmail.com>
2026-08-13 19:51:23 +0200

    Allow `label_test` to load raw issue text

--- label_test.py
+++ label_test.py
@@ -2,13 +2,17 @@
 import json
 import pprint
 import sys
+from contextlib import suppress
 
 from label import IssueBody, parse_issue
 
 
 def main() -> None:
     with open(sys.argv[1]) as f:
-        issue_body = json.load(f)['issue']['body']
+        issue_body = f.read()
+
+        with suppress(json.JSONDecodeError):
+            issue_body = json.loads(issue_body)['issue']['body']
 
     with open('.github/ISSUE_TEMPLATE/bugreport.yml') as f:
         issue_template = f.read()
